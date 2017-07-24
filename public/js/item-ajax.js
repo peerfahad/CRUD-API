931b41369e663af78adbb2caf70856bde56d5cc8
peerfahad <fahadrashid611@gmail.com>
2017-07-24 10:28:41 -0700

    Refactoring for more security and general design pattern

--- public/js/item-ajax.js
+++ public/js/item-ajax.js
@@ -5,6 +5,7 @@ var current_page = 1;
 var total_page = 0;
 var is_ajax_fire = 0;
 
+
 manageData();
 
 /* manage data list */
@@ -14,21 +15,21 @@ function manageData() {
         url: url+'api/getData.php',
         data: {page:page}
     }).done(function(data){
-    	total_page = Math.ceil(data.total/10);
-    	current_page = page;
+        total_page = Math.ceil(data.total/10);
+        current_page = page;
 
-    	$('#pagination').twbsPagination({
-	        totalPages: total_page,
-	        visiblePages: current_page,
-	        onPageClick: function (event, pageL) {
-	        	page = pageL;
+        $('#pagination').twbsPagination({
+            totalPages: total_page,
+            visiblePages: current_page,
+            onPageClick: function (event, pageL) {
+                page = pageL;
                 if(is_ajax_fire != 0){
-	        	  getPageData();
+                  getPageData();
                 }
-	        }
-	    });
+            }
+        });
 
-    	manageRow(data.data);
+        manageRow(data.data);
         is_ajax_fire = 1;
 
     });
@@ -37,42 +38,42 @@ function manageData() {
 
 /* Get Page Data*/
 function getPageData() {
-	$.ajax({
-    	dataType: 'json',
-    	url: url+'api/getData.php',
-    	data: {page:page}
-	}).done(function(data){
-		manageRow(data.data);
-	});
+    $.ajax({
+        dataType: 'json',
+        url: url+'api/getData.php',
+        data: {page:page}
+    }).done(function(data){
+        manageRow(data.data);
+    });
 }
 
 /* Add new Item table row */
 function manageRow(data) {
-	var	rows = '';
-	$.each( data, function( key, value ) {
-	  	rows = rows + '<tr>';
-	  	rows = rows + '<td>'+value.name+'</td>';
+    var rows = '';
+    $.each( data, function( key, value ) {
+        rows = rows + '<tr>';
+        rows = rows + '<td>'+value.name+'</td>';
         rows = rows + '<td>'+value.father_name+'</td>';
         rows = rows + '<td>'+value.email+'</td>';
-	  	rows = rows + '<td>'+value.phone+'</td>';
-	  	rows = rows + '<td data-id="'+value.id+'">';
+        rows = rows + '<td>'+value.phone+'</td>';
+        rows = rows + '<td data-id="'+value.id+'">';
         rows = rows + '<button data-toggle="modal" data-target="#edit-item" class="btn btn-primary edit-item">Edit</button> ';
         rows = rows + '<button class="btn btn-danger remove-item">Delete</button>';
         rows = rows + '</td>';
-	  	rows = rows + '</tr>';
-	});
+        rows = rows + '</tr>';
+    });
 
-	$("tbody").html(rows);
+    $("tbody").html(rows);
 }
 
 /* Create new Item */
 $(".crud-submit").click(function(e){
     e.preventDefault();
     var form_action = $("#create-item").find("form").attr("action");
-    var title = $("#create-item").find("input[name='name']").val();
-    var title = $("#create-item").find("input[name='father_name']").val();
-    var description = $("#create-item").find("textarea[name='email']").val();
-    var title = $("#create-item").find("input[name='phone']").val();
+    var name = $("#create-item").find("input[name='name']").val();
+    var father_name = $("#create-item").find("input[name='father_name']").val();
+    var email = $("#create-item").find("input[name='email']").val();
+    var phone = $("#create-item").find("input[name='phone']").val();
 
     if(name != '' && father_name != '' && email != '' && phone != ''){
         $.ajax({
@@ -83,7 +84,7 @@ $(".crud-submit").click(function(e){
         }).done(function(data){
             $("#create-item").find("input[name='name']").val('');
             $("#create-item").find("input[name='father_name']").val('');
-            $("#create-item").find("textarea[name='email']").val('');
+            $("#create-item").find("input[name='email']").val('');
             $("#create-item").find("input[name='phone']").val('');
             getPageData();
             $(".modal").modal('hide');
@@ -117,12 +118,14 @@ $("body").on("click",".remove-item",function(){
 $("body").on("click",".edit-item",function(){
 
     var id = $(this).parent("td").data('id');
-    var title = $(this).parent("td").prev("td").prev("td").text();
-    var description = $(this).parent("td").prev("td").text();
+    var name = $(this).parent("td").prev("td").prev("td").text();
+    var father_name = $(this).parent("td").prev("td").text();
+    var email = $(this).parent("td").prev("td").text();
+    var phone = $(this).parent("td").prev("td").text();
 
     $("#edit-item").find("input[name='name']").val(name);
     $("#edit-item").find("input[name='father_name']").val(father_name);
-    $("#edit-item").find("textarea[name='email']").val(email);
+    $("#edit-item").find("input[name='email']").val(email);
     $("#edit-item").find("input[name='phone']").val(phone);
     $("#edit-item").find(".edit-id").val(id);
 
@@ -133,10 +136,10 @@ $(".crud-submit-edit").click(function(e){
 
     e.preventDefault();
     var form_action = $("#edit-item").find("form").attr("action");
-    var title = $("#edit-item").find("input[name='name']").val();
-    var title = $("#edit-item").find("input[name='father_name']").val();
-    var title = $("#edit-item").find("input[name='email']").val();
-     var title = $("#edit-item").find("input[name='phone']").val();
+    var name = $("#edit-item").find("input[name='name']").val();
+    var father_name = $("#edit-item").find("input[name='father_name']").val();
+    var email = $("#edit-item").find("input[name='email']").val();
+    var phone = $("#edit-item").find("input[name='phone']").val();
 
     var id = $("#edit-item").find(".edit-id").val();
 
